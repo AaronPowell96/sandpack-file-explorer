0c5f226cc3157f006cdbac9eabae1711259bd483
Aaron Powell <apowell829@gmail.com>
2022-12-12 16:39:54 +0000

    fix: remove tests

--- tests/Example.test.tsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-
-// it('renders correctly', () => {
-//   const tree = renderer.create(<Example />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
