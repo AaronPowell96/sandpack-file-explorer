c9c9cdcbf7dfb90aa4918428541e872dd0a10321
Aaron Powell <apowell829@gmail.com>
2022-12-14 16:13:00 +0000

    chore: remove storybook webpack dep

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,11 +8,7 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-docs',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
   core: {
     builder: '@storybook/builder-vite',
   },
