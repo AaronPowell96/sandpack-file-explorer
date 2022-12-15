ec90a9e539a2a827a254ebd24b5a4306cb28bb48
Aaron Powell <apowell829@gmail.com>
2022-12-15 12:46:02 +0000

    chore: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,9 +12,15 @@ module.exports = {
       // Your production configuration goes here.
     }
     return mergeConfig(config, {
-      // optimizeDeps: {
-      //   include: ['@storybook/addon-essentials/preview.js'],
-      // },
+      optimizeDeps: {
+        include: [
+          '@storybook/addon-essentials/docs/mdx-react-shim',
+          '@codesandbox/sandpack-react',
+          '@codesandbox/sandpack-themes',
+          '@minoru/react-dnd-treeview',
+          'react-dnd',
+        ],
+      },
       // Your environment configuration here
     });
   },
