5d3e0e355dc437d8d2f0666dd37825dd9a4b9c0e
Aaron Powell <apowell829@gmail.com>
2022-12-15 13:11:04 +0000

    chore: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,31 @@
 // .storybook/main.js|ts
-const { mergeConfig } = require('vite');
-module.exports = {
-  stories: ['../stories'],
-  addons: ['@storybook/addon-essentials'],
-  core: {},
-  async viteFinal(config, { configType }) {
-    if (configType === 'DEVELOPMENT') {
-      // Your development configuration goes here
-    }
-    if (configType === 'PRODUCTION') {
-      // Your production configuration goes here.
-    }
-    return mergeConfig(config, {
-      optimizeDeps: {
-        include: [
-          '@storybook/addon-essentials/docs/mdx-react-shim',
-          '@codesandbox/sandpack-react',
-          '@codesandbox/sandpack-themes',
-          '@minoru/react-dnd-treeview',
-          'react-dnd',
-        ],
-      },
-      // Your environment configuration here
-    });
-  },
-  framework: {
-    name: '@storybook/react-vite',
-    options: {
-      fastRefresh: true,
+import { mergeConfig } from 'vite';
+export const stories = ['../stories'];
+export const addons = ['@storybook/addon-essentials'];
+export const core = {};
+export async function viteFinal(config, { configType }) {
+  if (configType === 'DEVELOPMENT') {
+    // Your development configuration goes here
+  }
+  if (configType === 'PRODUCTION') {
+    // Your production configuration goes here.
+  }
+  return mergeConfig(config, {
+    optimizeDeps: {
+      include: [
+        '@storybook/addon-essentials/docs/mdx-react-shim',
+        '@codesandbox/sandpack-react',
+        '@codesandbox/sandpack-themes',
+        '@minoru/react-dnd-treeview',
+        'react-dnd',
+      ],
     },
+    // Your environment configuration here
+  });
+}
+export const framework = {
+  name: '@storybook/react-vite',
+  options: {
+    fastRefresh: true,
   },
 };
